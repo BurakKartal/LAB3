939064a6b504e4213be8c3137ccf7a5d00ad0ea5
20090105016 <20090105016@muhlab12.lab.khas.edu.tr>
2012-10-02 09:08:58 -0430

    Drunkman Update

--- MonteCarloSimulation/src/Drunkman.java
+++ MonteCarloSimulation/src/Drunkman.java
@@ -1,9 +1,24 @@
+import java.util.Random;
 
 public class Drunkman {
 
-	public static void main(String[] args) {
-
-
+	public static void main(String[] args) {     
+		Random rand = new Random();    
+		int iState = rand.nextInt();    
+		double pValue = 0.60;     
+		int numSteps = rand.nextInt(100) + 1;   
+		int nSteps = 0;     
+		if (numSteps>=0) {        
+			System.out.println(iState);      
+			while (nSteps<numSteps) {           
+				if (rand.nextDouble() < pValue)             
+					iState++;            
+				else               
+					iState--;            
+				System.out.print(iState);       
+				nSteps++;
+			}
+		}
 	}
 
 }
